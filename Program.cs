1a0319ddf31b2f1e57e13c78e144a4595d34731e
JanielF <janielfloresdo@gmail.com>
2023-07-18 11:19:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Porfavor ingresar nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es : "+ nombre);
+
+        }
+    }
+}
