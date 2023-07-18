10cad65400f4053473e36f4525623a3a3bfea1d7
JanielF <janielfloresdo@gmail.com>
2023-07-18 11:27:22 -0400

    Ejercicio1 y 2.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,15 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            //Ejercicio1
             Console.Write("Porfavor ingresar nombre: ");
             string nombre = Console.ReadLine();
             Console.WriteLine("Su nombre es : "+ nombre);
+
+
+            //Ejercicio2
+            var fecha = DateTime.Now;
+            Console.WriteLine(fecha.ToString());
 
         }
     }
